67b025f7576aab22e4c030166bd80b4b2ef77b3e
Alejandro Munoz <aml8912@gmail.com>
2023-05-15 16:41:21 -0500

    vertical winning line is now working, also the borders of the grid are hidden

--- src/App.js
+++ src/App.js
@@ -12,10 +12,65 @@ function App() {
     const [resetGame,setResetGame]= useState(false)
 
     const setWinningLine = (winningPosition)=>{
-      const sum = winningPosition.reduce((accumulator, curr) => accumulator + curr, 0);
-      const xPos = sum / 3 * 100; // Assumes each square is 100px wide
-      const yPos = 50; // Position the line vertically in the middle of the row
-      setWinningLinePosition({ x: xPos, y: yPos });
+      // const sum = winningPosition.reduce((accumulator, curr) => accumulator + curr, 0);
+      const row = Math.floor(winningPosition[0] / 3); // Row index of the winning position
+      const widthSquare = 100; //100 pixel
+      let diagonalWinningLine = false;
+      const distanceBetweenFirstAndSecond = winningPosition[1]-winningPosition[0]
+      let xPos, yPos
+      console.log("distance between",distanceBetweenFirstAndSecond)
+      if((winningPosition[0]===0 && winningPosition[1]===4) ||(winningPosition[0]===2 && winningPosition[1]===4) ){
+        diagonalWinningLine=true
+      }
+      if (row === 0 && winningPosition[1]===1 ) {
+        //     // Horizontal line
+            xPos = widthSquare *0.5;
+            yPos = (row + 0.5) * widthSquare;
+      }else if(row === 0 && distanceBetweenFirstAndSecond===3){
+        console.log("enter vertical luine first row")
+        //vertical line
+        const transform =  "transform:    translateY(5%);"; 
+        const height = "280px";
+        const marginLeft = (widthSquare*0.5) + (winningPosition[0]*widthSquare);
+        setWinningLinePosition({
+          transform: "translateY(2%)",
+          height: "290px",
+          marginLeft: `${marginLeft-2}px`
+        })
+      }
+      // if(winningPosition[0] <3){ //first row
+      //   xPos = ((winningPosition[0]+1) * widthSquare)/2  
+      // }else if (winningLinePosition[0] <6){
+      //   xPos = ((winningPosition[0]+1) * widthSquare)/2 
+      // }
+      
+      // setWinningLinePosition({ x: xPos, y: yPos });
+      // const widthSquare = 100; // 100 pixel
+      // let xPos, yPos;
+    
+      // if (winningPosition.length > 0) {
+      //   const row = Math.floor(winningPosition[0] / 3); // Row index of the winning position
+      //   const col = winningPosition[0] % 3; // Column index of the winning position
+      //   if (row === 0) {
+      //     // Horizontal line
+      //     xPos = widthSquare / 2;
+      //     yPos = (row + 0.5) * widthSquare;
+      //   } else if (col === 0) {
+      //     // Vertical line
+      //     xPos = (col + 0.5) * widthSquare;
+      //     yPos = widthSquare / 2;
+      //   } else if (row === col) {
+      //     // Diagonal line from top-left to bottom-right
+      //     xPos = (col + 0.5) * widthSquare;
+      //     yPos = (row + 0.5) * widthSquare;
+      //   } else if (row + col === 2) {
+      //     // Diagonal line from top-right to bottom-left
+      //     xPos = (col + 1.5) * widthSquare;
+      //     yPos = (row + 0.5) * widthSquare;
+      //   }
+      // }
+    
+      // setWinningLinePosition({ x: xPos, y: yPos });
     }
     const handleNewGame = ()=>{
     setCells(["","","","","","","","",""]);
@@ -84,6 +139,7 @@ function App() {
         {winningLinePosition && (
       <div
         className="winning-line"
+        style={winningLinePosition}
         
       />
     )}
