ff8af1e776aa46c372211bde1720c96fa680cebc
Alejandro Munoz <aml8912@gmail.com>
2023-07-05 20:04:00 -0500

    hover for xshape and circleShape is ready

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,43 +1,40 @@
 import { useState } from "react";
-const Cell = ({ id, cells, turn, setCells, setTurn,winningMessage }) => {
-    const [turnShape, setTurnShape] = useState("");
-    console.log("winning message",winningMessage)
-    const handleOnClick = (event) => {
-        if (cells[id] == "") {
-            if (turn === "Circle") {
-                setTurnShape("circle")
-                setTurn("Cross")
-                handleChangeCellArray("circle")
-              
-            } else if (turn === "Cross") {
-                setTurnShape("x-shape")
-                setTurn("Circle")
-                handleChangeCellArray("cross")
-            }
-        }
-
+const Cell = ({ id, cells, turn, setCells, setTurn, winningMessage }) => {
+  const [turnShape, setTurnShape] = useState("");
+  console.log("winning message", winningMessage);
+  const handleOnClick = (event) => {
+    if (cells[id] == "") {
+      if (turn === "Circle") {
+        setTurnShape("circle");
+        setTurn("Cross");
+        handleChangeCellArray("circle");
+      } else if (turn === "Cross") {
+        setTurnShape("x-shape");
+        setTurn("Circle");
+        handleChangeCellArray("cross");
+      }
     }
-    const handleChangeCellArray = (shape) => {
+  };
+  const handleChangeCellArray = (shape) => {
+    const newCellsArray = cells.map((cellValue, index) => {
+      if (index === id) {
+        return shape;
+      } else {
+        return cellValue;
+      }
+    });
+    setCells(newCellsArray);
+  };
 
-        const newCellsArray = cells.map((cellValue, index) => {
-            if (index === id) {
-                return shape;
-            } else {
-                return cellValue;
-            }
-        })
-        setCells(newCellsArray)
-    }
-
-   
-    return (
-        <div className={`square${turnShape? ` ${turnShape}Active`:` ${turn}Hover`}`} id={id} onClick={ !winningMessage ? handleOnClick:undefined}>
-            {
-                <div className={turnShape?`${turnShape}`:``}></div>
-            }
-          
-        </div>
-    )
-}
+  return (
+    <div
+      className={`square${turnShape ? ` ${turnShape}Active` : ""}`}
+      id={id}
+      onClick={!winningMessage ? handleOnClick : undefined}
+    >
+      {<div className={turnShape ? `${turnShape}` : ` ${turn}Hover`}></div>}
+    </div>
+  );
+};
 
 export default Cell;
